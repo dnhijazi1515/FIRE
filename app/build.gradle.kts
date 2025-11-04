171c85dc3a69e6d8f6dbefedd3edff01070274fa
PC-14 <dnhijazi15108@gmail.com>
2025-11-04 09:45:10 +0200

    fir

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.play.services.tasks)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/pro1adan/MainActivity3.java
+++ app/src/main/java/com/example/pro1adan/MainActivity3.java
@@ -1,18 +1,62 @@
 package com.example.pro1adan;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnFailureListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity3 extends AppCompatActivity {
+    FirebaseFirestore db;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        btn=findViewById(R.id.add);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> client = new HashMap<>();
+                client.put("Name", "Ahmad");
+                client.put("Age", 25);
+                client.put("Address", "Tamra");
+                client.put("Phone", "0526485956");
+
+                db.collection("Clients")
+                        .add(client)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            }
+        });
+
     }
 }
