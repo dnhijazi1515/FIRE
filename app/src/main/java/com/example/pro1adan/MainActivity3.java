4a25ddb64d4d4b9845ba5330baf545f623b50388
PC-14 <dnhijazi15108@gmail.com>
2025-11-05 08:56:50 +0200

    fir11

--- app/src/main/java/com/example/pro1adan/MainActivity3.java
+++ app/src/main/java/com/example/pro1adan/MainActivity3.java
@@ -1,18 +1,12 @@
 package com.example.pro1adan;
 
-import static android.content.ContentValues.TAG;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
@@ -24,17 +18,23 @@ import java.util.Map;
 
 public class MainActivity3 extends AppCompatActivity {
     FirebaseFirestore db;
+    EditText name,add,phoneNum,age;
     Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        btn=findViewById(R.id.add);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
+        btn=findViewById(R.id.addbtn);
+        name=findViewById(R.id.na);
+        add=findViewById(R.id.addr);
+        phoneNum=findViewById(R.id.phoneNum);
+        age=findViewById(R.id.ageEd);
         db = FirebaseFirestore.getInstance();
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Map<String, Object> client = new HashMap<>();
                 client.put("Name", "Ahmad");
                 client.put("Age", 25);
@@ -46,13 +46,12 @@ public class MainActivity3 extends AppCompatActivity {
                         .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                             @Override
                             public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error adding document", e);
                             }
                         });
             }
